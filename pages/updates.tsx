1e3130eb8222a6dc8df517dfdb509a1b45669ad7
miccou <30512204+miccou@users.noreply.github.com>
2023-04-24 01:53:06 +0000

    wip

--- pages/updates.tsx
+++ pages/updates.tsx
@@ -1,10 +1,11 @@
-import Content from "./content";
-import DateBrowser from "./date-browser";
+import React from "react";
+import Content from "../components/content";
+import DateBrowser from "../components/date-browser";
 
 export default function Home() {
   return (
     <div className="h-screen w-screen flex flex-col">
-      <div className="w-screen flex-1 ">
+      <div className="w-screen flex flex-col flex-1 ">
         <Content />
       </div>
       <div className="w-screen h-fit ">
